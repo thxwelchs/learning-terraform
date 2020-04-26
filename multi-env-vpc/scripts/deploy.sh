58e22e427a513568ae58bcf00285a4b2e619382a
thxwelchs <thxwelchs@gmail.com>
2020-04-26 22:50:19 +0900

    cicd dockerize

--- multi-env-vpc/scripts/deploy.sh
+++ multi-env-vpc/scripts/deploy.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # frontend deploy
-if [ -f /home/ec2-user/build/frontend-dist/index.html ]; then
-#  if [ "$(sudo docker inspect -f "{{ .State.Running }}" web)" == "true" ]; then
-#  fi
-  sudo cp -r /home/ec2-user/build/frontend-dist/* /usr/share/nginx/html/
-fi
+#if [ -f /home/ec2-user/build/frontend-dist/index.html ]; then
+##  if [ "$(sudo docker inspect -f "{{ .State.Running }}" web)" == "true" ]; then
+##  fi
+#  sudo cp -r /home/ec2-user/build/frontend-dist/* /usr/share/nginx/html/
+#fi
 
 # backend deploy
