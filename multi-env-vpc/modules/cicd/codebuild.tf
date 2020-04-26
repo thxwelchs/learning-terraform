af9da0439acac741808590be6c96d0060a105f15
thxwelchs <thxwelchs@gmail.com>
2020-04-27 00:11:55 +0900

    ci test

--- multi-env-vpc/modules/cicd/codebuild.tf
+++ multi-env-vpc/modules/cicd/codebuild.tf
@@ -8,8 +8,9 @@ resource "aws_codebuild_project" "project" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/amazonlinux2-x86_64-standard:3.0"
+    image        = "aws/codebuild/standard:2.0"
     type         = "LINUX_CONTAINER"
+    privileged_mode = true
     environment_variable {
       name = "ENV_NAME"
       value = var.name
